c5416952a48e43ce14d4507d4c8cbae99208e4e0
Jamie Graham <jagraham04@yahoo.com>
2023-06-05 15:37:51 +0100

    Created moment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_143042) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_143616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.string "title"
+    t.date "date_start"
+    t.date "date_end"
+    t.bigint "shrine_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shrine_id"], name: "index_chapters_on_shrine_id"
+  end
 
   create_table "shrine_users", force: :cascade do |t|
     t.bigint "shrine_id", null: false
@@ -44,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_143042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chapters", "shrines"
   add_foreign_key "shrine_users", "shrines"
   add_foreign_key "shrine_users", "users"
 end
