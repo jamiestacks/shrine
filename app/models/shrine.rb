e4bb7bba54c962c78b5cb589b7a4cdcf3020c18d
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-06 15:17:52 +0100

    Validations for model shrine finished and tested

--- app/models/shrine.rb
+++ app/models/shrine.rb
@@ -3,4 +3,12 @@ class Shrine < ApplicationRecord
   has_many :chapters
   has_many :messages
 
+  validates :name, :dob, presence: true
+  validate :birth_cannot_be_in_the_future
+
+  def birth_cannot_be_in_the_future
+    if dob.present? && dob > Date.today
+      errors.add(:dod, "Can't be in the past")
+    end
+  end
 end
