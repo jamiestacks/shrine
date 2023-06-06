13e59ab5b7f940dbd19f404388f47d93984c904a
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-06 16:44:18 +0100

    Initial validations completed

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,14 +2,14 @@ class Chapter < ApplicationRecord
   belongs_to :shrine
   has_many :stories
 
-  validates :title, presence: true
+  validates :title, :date_start, :date_end, presence: true
   validate :within_dob_dod
 
   def within_dob_dod
-    if date_start < dob
-      erros.add(:date_start, :before_birth, message: "Date has to be within the lifespan")
-    elsif date_end > dod
-      erros.add(:date_end, :after_death, message: "Date has to be within the lifespan")
+    if date_start < self.shrine.dob
+      errors.add(:date_start, :before_birth, message: "Date has to be within the lifespan")
+    elsif date_end > self.shrine.dod
+      errors.add(:date_end, :after_death, message: "Date has to be within the lifespan")
     elsif date_start > date_end
       errors.add(:date_start, :invalid_start_date, message: "The start date cannot be before the end date")
       errors.add(:date_end, :invalid_end_date, message:  "The end date cannot be after the start date")
