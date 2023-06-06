57cf73894272fa27c2c2f371cdb58593987a2804
Jamie Graham <jagraham04@yahoo.com>
2023-06-06 14:42:12 +0100

    committing latest changes

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,29 @@
 class ChaptersController < ApplicationController
+  before_action :set_shrine, only: %i[new]
+
+  def new
+    @chapter = Chapter.new
+    @shrine = Shrine.where(shrine: @shrine)
+  end
+
+  def create
+    @shrine = Shrine.find(params[:shrine_id])
+    @chapter = Chapter.new(chapter_params)
+    @chapter.shrine = @shrine
+    if @chapter.save
+      redirect_to shrine_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_shrine
+    @shrine = Shrine.find(params[:shrine_id])
+  end
+
+  def chapter_params
+    params.require(:chapter).permit(:title, :date_start, :date_end, :shrine_id)
+  end
 end
