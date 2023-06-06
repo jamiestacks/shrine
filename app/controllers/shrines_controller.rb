dba6dcb07ab32842fba11fc75da8cc3f2d670dfb
hasna begum <h.b.muni16@gmail.com>
2023-06-06 15:25:27 +0100

    added show and index page for shrine

--- app/controllers/shrines_controller.rb
+++ app/controllers/shrines_controller.rb
@@ -1,2 +1,17 @@
 class ShrinesController < ApplicationController
+  def index
+    @shrines = Shrine.all
+  end
+
+  def show
+    @shrine = Shrine.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
