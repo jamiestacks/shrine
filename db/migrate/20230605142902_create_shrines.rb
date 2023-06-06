7566655529ec193a92f2eb8d2fd225e36084d5b8
hasna begum <h.b.muni16@gmail.com>
2023-06-06 12:39:00 +0100

    fixed shrine class, changed from Module to Class by changing module name to MyShrine

--- db/migrate/20230605142902_create_shrines.rb
+++ db/migrate/20230605142902_create_shrines.rb
@@ -4,6 +4,7 @@ class CreateShrines < ActiveRecord::Migration[7.0]
       t.string :name
       t.date :DOB
       t.date :DOD
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
