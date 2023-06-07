3747b6dccf0d7100780d5e30dd8ae54c9fdd7246
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-07 11:35:10 +0100

    Search bar added to invites

--- app/controllers/shrine_users_controller.rb
+++ app/controllers/shrine_users_controller.rb
@@ -1,2 +1,12 @@
 class ShrineUsersController < ApplicationController
+
+  def new
+    @user = current_user
+    @invite = ShrineUser.new
+    @invite.user_id = @user
+  end
+
+  def create
+
+  end
 end
