e4e4d82f825601a3e2038d72b64e8519b8f4daab
hasna begum <h.b.muni16@gmail.com>
2023-06-07 10:32:44 +0100

    finished messages crete and messages param method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,21 @@ class MessagesController < ApplicationController
   end
 
   def create
-
+    @shrine = Shrine.find(params[:shrine_id])
+    @message = Message.new(message_params)
+    @message.shrine = @shrine
+    @message.user = current_user
+    if @message.save
+      redirect_to shrine_messages_path(@shrine)
+    else
+      render "messages/index", status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
 end
