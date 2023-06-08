dee493d3ea9010da317be8345a25a24cabb70232
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-08 17:48:28 +0100

    last master added to branch

--- db/migrate/20230607134258_add_status_to_shrine_users.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToShrineUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :shrine_users, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_110921) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_135004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_110921) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
+    t.string "status", default: "0"
+    t.string "relationship"
     t.index ["shrine_id"], name: "index_shrine_users_on_shrine_id"
     t.index ["user_id"], name: "index_shrine_users_on_user_id"
   end
