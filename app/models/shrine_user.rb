c69480ad0da847e053d204305ecd18e6dd0673c6
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-09 10:45:25 +0100

    ShrineUser model changed to accept string instead o intger enum

--- app/models/shrine_user.rb
+++ app/models/shrine_user.rb
@@ -3,5 +3,5 @@ class ShrineUser < ApplicationRecord
   belongs_to :user
 
   validates :user, uniqueness: { scope: :shrine, message: "You have already invited this user" }
-  enum :status, [ :pending, :accept, :decline ]
+  enum :status, { pending: "0", accept: "1", decline: "2" }
 end
