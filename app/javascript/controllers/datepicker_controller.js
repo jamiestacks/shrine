4a5afd6184ab1c2f5db4534f10bc25856a745c5b
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-06-15 13:05:09 +0100

    Minor changes to css

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,8 +3,11 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+
   connect() {
     console.log("connected")
     flatpickr(this.element)
+
   }
+
 }
